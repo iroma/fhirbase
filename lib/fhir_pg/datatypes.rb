95923b35781c3ab763f6fb7afca627a2d9fd49ba
niquola <niquola@gmail.com>
2013-11-12 15:38:52 +0400

    start mount

--- lib/fhir_pg/datatypes.rb
+++ lib/fhir_pg/datatypes.rb
@@ -6,13 +6,23 @@ module FhirPg
     end
 
     def mk_db(xml)
-      name_idx = index_nodes_by_name
+      idx = index_nodes_by_name(xml)
+      enums = collect_enums(idx)
+      primitives = collect_primitives(idx)
+      types = collect_types(idx, enums, primitives)
+      {}.merge(enums).merge(primitives).merge(types)
+    end
 
-      {}.tap do |db|
-        db.merge(collect_enums(idx))
-        db.merge(collect_simple_types(idx))
-        db.merge(collect_complex_types(idx))
-      end
+    def mount(db, parent_path, key, type)
+      tp = db[type]
+      path = "#{parent_path}.#{key}"
+      tp.dup.merge(
+        path: path,
+        attrs: mount_recur(path, tp[:attrs])
+      )
+    end
+
+    def mount_recur(path, attrs)
     end
 
     def nodes(xml)
@@ -29,7 +39,7 @@ module FhirPg
     def collect_enums(idx)
       list_names = idx.keys.select{|n| n.to_s =~/_list$/}
       mk_index(list_names) do |list_key|
-        key = list_key.to_s.gsub(/_list$/,'').to_sym
+        key = unpostfix(list_key, 'list')
         node =  idx[key]
         list_node = idx[list_key]
         [key, mk_enum(key, list_node)] if node.present?
@@ -47,19 +57,78 @@ module FhirPg
     def collect_primitives(idx)
       names = idx.keys.select{|n| n.to_s =~/_primitive$/}
       mk_index(names) do |p_key|
-        key = p_key.to_s.gsub(/_primitive$/,'').to_sym
+        key = unpostfix(p_key, 'primitive')
         node =  idx[key]
         p_node = idx[p_key]
         [key, mk_primitive(key, p_node)] if node.present?
       end
     end
 
-    def mk_primitive(kye, p_node)
+    def mk_primitive(key, p_node)
       {
         name: key,
         kind: :primitive,
         type: type(p_node)
       }
+    end
+
+    def unpostfix(str, *postfix)
+      str = str.to_s
+      postfix.each do |pfx|
+        str.gsub!(/_#{pfx}$/,'')
+      end
+      str.to_sym
+    end
+
+    def collect_types(idx, enums, primitives)
+      tmp_types = mk_index(idx) do |(key, node)|
+        key = unpostfix(key, 'primitive', 'list')
+        next if enums[key]
+        next if primitives[key]
+        [key, mk_type(key, node)] if key
+      end
+
+      fix_complex_types(tmp_types, enums, primitives)
+    end
+
+    # dirty algorighm with temporal lookup
+    def fix_complex_types(tmp_types, enums, primitives)
+      mk_index(tmp_types) do |(key, tp)|
+        [
+          key,
+          tp.dup.merge(attrs: fix_attrs(tp[:attrs], tmp_types, enums, primitives))
+        ]
+      end
+    end
+
+    def fix_attrs(attrs, tmp_types, enums, primitives)
+      mk_index(attrs) do |(key, props)|
+        type = props[:type].to_sym
+        tp = primitives[type] || enums[type] || tmp_types[type]
+        raise "Could not find type #{key}: #{props}" unless tp.present?
+        [key, tp.dup.merge(props)]
+      end
+    end
+
+    def mk_type(key, node)
+      {
+        name: key,
+        kind: :complex_type,
+        type: key,
+        attrs: initial_attrs(key, node)
+      }
+    end
+
+    def initial_attrs(key, node)
+      mk_index(node.xpath('.//element')) do |child_node|
+        child_key = normalize_name(child_node[:name])
+        [child_key, {
+          name: child_key,
+          type: normalize_name(child_node[:type]),
+          requied: requied?(child_node),
+          collection: collection?(child_node)
+        }]
+      end
     end
 
     private
@@ -74,7 +143,7 @@ module FhirPg
     end
 
     def type(node)
-      normalize_name(node[:type])
+      normalize_name(node[:type]).to_sym
     end
 
     def requied?(node)
@@ -89,12 +158,8 @@ module FhirPg
       "#{parent_path}.#{child}"
     end
 
-    def child_path(parent_path, child)
-      "#{parent_path}.#{child}"
-    end
-
     def normalize_name(str)
-      str.to_s.underscore
+      str.to_s.underscore.to_sym
     end
 
     extend self
--- spec/datatypes_spec.rb
+++ spec/datatypes_spec.rb
@@ -30,15 +30,45 @@ describe FhirPg::Datatypes do
 
   it "#collect_primitive" do
     idx = subject.index_nodes_by_name(xml)
-    enums = subject.collect_enums(idx)
-    nu = enums[:name_use]
-    nu[:kind] == :enum
-    nu[:name] == :name_use
-    nu[:options].should include('usual')
+    items = subject.collect_primitives(idx)
+    str = items[:string]
+    str[:name] == :string
+    str[:kind] == :primitive
+    str[:type] == 'string'
   end
 
-  example do
-    pending
+  it "#collect_types" do
+    idx = subject.index_nodes_by_name(xml)
+    items = subject.collect_types(idx,
+                                  subject.collect_enums(idx),
+                                  subject.collect_primitives(idx))
+    cc = items[:codeable_concept]
+    cc[:name].should == :codeable_concept
+    cc[:kind].should == :complex_type
+    cc[:type].should == :codeable_concept
+    attrs = cc[:attrs]
+    cd =  attrs[:coding]
+    cd[:name].should == :coding
+    cd[:kind].should == :complex_type
+    cd[:type].should == :coding
+    txt = cc[:attrs][:text]
+    txt[:kind].should == :primitive
+    txt[:type].should == :string
+  end
+
+  it "#mk_db" do
     db = subject.mk_db(xml)
+    db[:codeable_concept].should_not be_nil
+    db[:string].should_not be_nil
+    db[:address].should_not be_nil
+  end
+
+  it "mount" do
+    db = subject.mk_db(xml)
+    tree = subject.mount(db, 'user.name', 'gender', :codeable_concept)
+    tree[:path].should == 'user.name.gender'
+    tree[:type].should == :codeable_concept
+    tree[:attrs][:coding][:path].should == 'user.name.gender.coding'
+    tree[:attrs][:coding][:collection].should be_true
   end
 end
