4c527fa30336e0a92e6c2159d640531afd47281d
Maksym Bodnarchuk <bodnarchuk@gmail.com>
2014-01-27 22:05:14 +0200

    add extension sample

--- fhirbase_spec.sql
+++ fhirbase_spec.sql
@@ -2,10 +2,12 @@
 --{{{
 \set pt1 `cat ./spec/pt1.json`
 \set pt2 `cat ./spec/pt2.json`
+\set ext `cat ./spec/extension.json`
 
 \timing
 select count(*) from (select insert_resource(:'pt1') from generate_series(1,10)) gen;
 select count(*) from (select insert_resource(:'pt2') from generate_series(1,10)) gen;
+select count(*) from (select insert_resource(:'ext') from generate_series(1,10)) gen;
 
 do language plv8 $$
   plv8.elog(NOTICE, 'HELLO')
