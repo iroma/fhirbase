a8bfc4d1dffc93556e4150db53084b6cee2a241c
Maksym Bodnarchuk <bodnarchuk@gmail.com>
2014-02-03 14:44:36 +0200

    progress

--- dev/install.sql
+++ dev/install.sql
@@ -5,3 +5,15 @@ drop schema if exists meta cascade;
 \ir meta/load_plv8_modules.sql
 \ir meta/functions.sql
 \ir meta/views.sql
+\ir meta/resource_generation.sql
+do language plv8 $$
+  load_module('schema')
+  sql.generate_schema('0.12')
+$$;
+create OR replace function public.insert_resource(json json)
+returns void
+language plv8
+as $$
+  load_module('medapp');
+  sql.insert_resource(json)
+$$;
