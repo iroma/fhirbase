24d322f0cc70519b8c94a33a13db6475db5fe8c4
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2014-02-07 15:38:53 +0400

    fbdev.sh install/build commands

--- dev/fbdev.sh
+++ dev/fbdev.sh
@@ -1,9 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
 . ./fbdev_config.sh
 
+cd $FHIRBASE_HOME;
+
+function install_cmd {
+    if [ -z "$1" ]; then
+        echo "install command requires dbname argument"
+        exit
+    else
+        coffee --compile --output js js_src
+        $PG_BIN_DIR/psql $PSQL_ARGS -d $1 < install.sql
+    fi
+}
+
 case "$1" in
     "install" )
-        coffee --compile --output js js_src
-        psql $PSQL_ARGS < install.sql
+        install_cmd $2
+        ;;
+    "build" )
+        # echo "DROP DATABASE $BUILD_DB_NAME; CREATE DATABASE $BUILD_DB_NAME;" | $PG_BIN_DIR/psql $PSQL_ARGS -d postgres;
+        # install_cmd $BUILD_DB_NAME
+        $PG_BIN_DIR/pg_dump $PSQL_ARGS --format=plain --schema=fhirr $BUILD_DB_NAME > $FHIRBASE_HOME/../fhirbase.sql &&
+          echo "FhirBase schema successfully dumped to $FHIRBASE_HOME/../fhirbase.sql"
 esac
