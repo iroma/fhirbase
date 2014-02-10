34a2d52719fd69ad94219cbabd79449f61991083
nicola <niquola@gmail.com>
2014-02-10 12:42:01 +0400

    progress

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -1,25 +1,21 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y git build-essential
+#wget https://alioth.debian.org/scm/loggerhead/pkg-postgresql/postgresql-common/trunk/download/head:/apt.postgresql.org.s-20130224224205-px3qyst90b3xp8zj-1/apt.postgresql.org.sh
+#chmod u+x apt.postgresql.org.sh
+sudo /home/vagrant/fhirbase/apt.postgresql.org.sh
 
-wget https://alioth.debian.org/scm/loggerhead/pkg-postgresql/postgresql-common/trunk/download/head:/apt.postgresql.org.s-20130224224205-px3qyst90b3xp8zj-1/apt.postgresql.org.sh
-chmod u+x apt.postgresql.org.sh
-sudo ./apt.postgresql.org.sh
+sudo apt-get install -y git build-essential libv8-dev libpq-dev postgresql-9.3 postgresql-contrib-9.3 postgresql-server-dev-9.3
 
-sudo apt-get install -y postgresql-9.3 postgresql-contrib-9.3 postgresql-server-dev-9.3
-sudo apt-get install -y libv8-dev libpq-dev
-
-test -d plv8js || git clone https://code.google.com/p/plv8js/
-cd plv8js
+test -d /tmp/plv8js || git clone https://code.google.com/p/plv8js/ /tmp/plv8js
+cd /tmp/plv8js
 sudo make
 sudo make install
 
-cd
-
-sudo su postgres -c createuser -s root
+sudo su postgres -c 'createuser -s root'
+sudo su postgres -c 'createuser -s vagrant'
 
 sudo perl -MCPAN -e 'install TAP::Parser::SourceHandler::pgTAP'
-test -d pgtap || git clone https://github.com/theory/pgtap.git
-cd pgtap
+test -d /tmp/pgtap || git clone https://github.com/theory/pgtap.git /tmp/pgtap
+cd /tmp/pgtap
 sudo make installcheck
 sudo make install
