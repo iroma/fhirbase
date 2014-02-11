7fb9b8368da25da519b2739541b811e5520ebe65
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2014-02-11 12:47:44 +0400

    formatted JSON fixture

--- fhirbase_test.sql
+++ fhirbase_test.sql
@@ -2,7 +2,7 @@
 \set pt2 `cat ./test/pt2.json`
 
 \timing
-SELECT COUNT(*) FROM (SELECT fhir.insert_resource(:'pt1') FROM generate_series(1,10)) gen;
+-- SELECT COUNT(*) FROM (SELECT fhir.insert_resource(:'pt1') FROM generate_series(1,10)) gen;
 SELECT COUNT(*) FROM (SELECT fhir.insert_resource(:'pt2') FROM generate_series(1,10)) gen;
 
 /* SELECT * FROM fhir.patient_name;*/
