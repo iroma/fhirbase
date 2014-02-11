3f22db991f44647222b7939b7c2e2160996725b4
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2014-02-11 12:39:13 +0400

    missing build tests

---
+++ fhirbase_test.sql
@@ -0,0 +1,8 @@
+\set pt1 `cat ./test/pt1.json`
+\set pt2 `cat ./test/pt2.json`
+
+\timing
+SELECT COUNT(*) FROM (SELECT fhir.insert_resource(:'pt1') FROM generate_series(1,10)) gen;
+SELECT COUNT(*) FROM (SELECT fhir.insert_resource(:'pt2') FROM generate_series(1,10)) gen;
+
+/* SELECT * FROM fhir.patient_name;*/
