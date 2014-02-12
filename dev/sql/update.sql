d4ee53cbec9fb139c246b58871513a9d86fd9fbf
Maksym Bodnarchuk <bodnarchuk@gmail.com>
2014-02-12 14:40:04 +0200

    improve tests

--- dev/sql/update.sql
+++ dev/sql/update.sql
@@ -1,7 +1,12 @@
 create or replace function fhir.update_resource(id uuid, jdata json) returns integer language plpgsql as $$
-  BEGIN
-					perform fhir.delete_resource(id);
-					perform fhir.insert_resource(jdata);
-					return 0::integer;
-  END
+DECLARE
+res integer;
+BEGIN
+				select fhir.delete_resource(id) into res;
+				IF res = 0 THEN
+								raise exception 'Resource with id % not found', id;
+				END IF;
+				perform fhir.insert_resource(jdata);
+				return 0::integer;
+	END
 $$;
--- dev/test/update_test.sql
+++ dev/test/update_test.sql
@@ -7,7 +7,7 @@
 \set two_json `cat $FHIRBASE_HOME/test/fixtures/patient_two.json`
 
 BEGIN;
-SELECT plan(2);
+SELECT plan(3);
 
 \timing
 select fhir.insert_resource(:'pt_json'::json) as resource_id \gset
@@ -19,6 +19,14 @@ select fhir.update_resource(:'resource_id', :'two_json'::json);
 
 SELECT is(text::varchar, 'Gavrila'::varchar, 'insert patient')
        FROM fhir.patient_name;
+
+---
+select uuid_generate_v4() as uuid \gset
+PREPARE my_thrower AS select fhir.update_resource(:'uuid', :'two_json'::json);
+SELECT throws_ok(
+		'my_thrower',
+ 'Resource with id ' || :'uuid'::varchar || ' not found'
+);
 
 SELECT * FROM finish();
 ROLLBACK;
