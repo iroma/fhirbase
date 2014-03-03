52b9e89e5b8681db293a40b99f299e5d89ae0b1e
nicola <niquola@gmail.com>
2014-03-03 19:17:25 +0400

    style it

--- dev/sql/06_datatypes.sql
+++ dev/sql/06_datatypes.sql
@@ -1,88 +1,102 @@
 set search_path = fhir, pg_catalog;
-create view meta.enums as (     --
-  select replace(datatype, '-list','') as enum, array_agg(value) as options
-  from meta.datatype_enums
-  group by replace(datatype, '-list','')
+
+CREATE VIEW meta.enums AS (
+  SELECT  replace(datatype, '-list','')
+      AS  enum
+          ,array_agg(value)
+      AS  options
+    FROM  meta.datatype_enums
+GROUP BY  replace(datatype, '-list','')
 );
 
 CREATE VIEW meta.primitive_types as (
-  SELECT type, pg_type
-  FROM meta.type_to_pg_type
-  UNION
-  SELECT enum, 'fhir."' || enum  || '"'-- HACK
-  FROM meta.enums
+  SELECT type
+         ,pg_type
+    FROM meta.type_to_pg_type
+   UNION SELECT enum, 'fhir."' || enum  || '"'
+    FROM meta.enums
 );
 
-CREATE OR REPLACE
-VIEW meta._datatype_unified_elements AS (
-  SELECT
-    ARRAY[datatype, name] as path,
-    type,
-    min_occurs as min,
-    case
-      when max_occurs = 'unbounded'
-        then '*'
-      else max_occurs
-    end as max
-  FROM meta.datatype_elements
-  where datatype <> 'Resource'
+CREATE VIEW meta._datatype_unified_elements AS (
+  SELECT ARRAY[datatype, name]
+      AS path
+         ,type
+         ,min_occurs
+      AS min
+         ,CASE when max_occurs = 'unbounded'
+           THEN '*'
+           ELSE max_occurs
+          END
+      AS max
+    FROM meta.datatype_elements
+   WHERE datatype <> 'Resource'
 );
 
-create or replace
-view meta.datatype_unified_elements as (
-  with recursive tree(
-    path, type, min, max
-  ) as (
-    select r.* from meta._datatype_unified_elements r
-    union
-    select
-      t.path || ARRAY[array_last(r.path)] as path,
-      r.type as type,
-      t.min as min,
-      t.max as max
-    from meta._datatype_unified_elements r
-    join tree t on t.type = r.path[1]
+CREATE VIEW meta.datatype_unified_elements as (
+  WITH RECURSIVE tree(
+    path
+    ,type
+    ,min
+    ,max
+  ) AS (
+    SELECT r.* FROM meta._datatype_unified_elements r
+    UNION
+    SELECT t.path || ARRAY[array_last(r.path)] as path,
+           r.type as type,
+           t.min as min,
+           t.max as max
+      FROM meta._datatype_unified_elements r
+      JOIN tree t on t.type = r.path[1]
   )
-  select * from tree t limit 1000
+  SELECT * FROM tree t LIMIT 1000
 );
 
 CREATE VIEW meta.unified_complex_datatype AS (
-  select
-    ue.path as path,
-    coalesce(tp.type, ue.path[1]) as type
-    from (
-      select array_pop(path) as path
-      from meta.datatype_unified_elements
-      group by array_pop(path)
-    ) ue
-  LEFT JOIN meta.datatype_unified_elements tp
-  on tp.path = ue.path
+  SELECT   ue.path as path
+           ,coalesce(tp.type, ue.path[1]) as type
+     FROM  (
+              SELECT array_pop(path)
+                  AS path
+                FROM meta.datatype_unified_elements
+            GROUP BY array_pop(path)
+           )
+       AS  ue
+LEFT JOIN meta.datatype_unified_elements tp
+       ON tp.path = ue.path
 );
 
 CREATE VIEW meta.unified_datatype_columns AS (
-  SELECT dt.*,
-    pt.pg_type as pg_type,
-    column_ddl(array_last(dt.path), pt.pg_type, dt.min, dt.max) as column_ddl
-  FROM  meta.datatype_unified_elements dt
-  JOIN meta.primitive_types pt ON underscore(pt.type) = underscore(dt.type)
-  where array_length(dt.path,1) = 2
+  SELECT  dt.*
+          ,pt.pg_type as pg_type
+          ,column_ddl(array_last(dt.path) ,pt.pg_type, dt.min, dt.max)
+      AS  column_ddl
+    FROM  meta.datatype_unified_elements dt
+    JOIN  meta.primitive_types pt
+      ON  underscore(pt.type) = underscore(dt.type)
+   WHERE  array_length(dt.path,1) = 2
 );
 
-CREATE OR REPLACE
-VIEW meta.datatype_tables AS (
-  SELECT
-    table_name(path) as table_name,
-    case
-    when array_length(path, 1) = 1 then 'resource_component'
-    else table_name(ARRAY[type])
-    end as base_table,
-    (SELECT coalesce(array_agg(column_ddl), ARRAY[]::varchar[])
-      FROM meta.unified_datatype_columns cls
-      WHERE array_pop(cls.path) = cd.path
-    ) as columns,
-    *
-  FROM  meta.unified_complex_datatype cd
-  order by array_length(cd.path,1), table_name
+CREATE VIEW meta.datatype_tables AS (
+  SELECT  table_name(path)
+      AS  table_name
+          ,CASE WHEN array_length(path, 1) = 1
+            THEN 'resource_component'
+            ELSE table_name(ARRAY[type])
+          END
+      AS  base_table
+          ,(
+              SELECT coalesce(
+                      array_agg(column_ddl),
+                      ARRAY[]::varchar[]
+                     )
+                 FROM meta.unified_datatype_columns cls
+                WHERE array_pop(cls.path) = cd.path
+            )
+      AS  columns
+          ,*
+    FROM  meta.unified_complex_datatype cd
+ORDER BY  array_length(cd.path,1)
+          ,table_name
 );
 
 set search_path = public, pg_catalog;
