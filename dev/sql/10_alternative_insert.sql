871e01df0055d6068cbf8b651977301310779759
nicola <niquola@gmail.com>
2014-03-05 18:50:06 +0400

    Patient inserted!

--- dev/sql/10_alternative_insert.sql
+++ dev/sql/10_alternative_insert.sql
@@ -20,44 +20,45 @@ $$;
 DROP FUNCTION IF EXISTS build_insert_satement(text,json, text, text, text);
 CREATE OR REPLACE
 FUNCTION build_insert_satement(
-    table_name text,
+    _table_name text,
     obj json,
     id text,
     parent_id text,
-    resource_id text)
+    resource_id text
+)
 RETURNS text
 AS $$
+WITH vals AS (
+  SELECT fhir.underscore(a.key) as key,
+         a.value as value,
+         dc.data_type as data_type
+    FROM json_each_text(obj) a,
+         information_schema.columns dc
+    WHERE fhir.underscore(a.key) = dc.column_name::text
+        AND dc.table_name = _table_name
+        AND dc.table_schema = 'fhir'
+        AND dc.data_type IS NOT NULL
+), key_vals AS (
+    SELECT vals.key as key,
+           CASE WHEN vals.data_type = 'ARRAY'
+            THEN json_array_to_array_literal(vals.value::json)
+            ELSE quote_literal(vals.value)
+          END AS value
+      FROM vals
+      UNION
+        SELECT 'id' AS key, quote_literal(id) AS value
+      UNION
+        SELECT 'parent_id' AS key, quote_literal(parent_id) AS value
+        WHERE parent_id IS NOT NULL
+      UNION
+        SELECT 'resource_id' AS key, quote_literal(resource_id) AS value
+        WHERE parent_id IS NOT NULL AND resource_id <> id
+)
 select 'insert into '
-   || 'fhir.' || table_name
-   || ' (' || string_agg(fhir.underscore(b.key),',') || ') '
-   || ' VALUES (' || string_agg(b.value, ',') || ')'
-   from (
-        WITH vals AS (
-          SELECT fhir.underscore(a.key) as key,
-                 a.value as value,
-                 dc.data_type as data_type
-            FROM json_each_text(obj) a
-             INNER JOIN information_schema.columns dc
-                ON a.key = dc.column_name::text
-                AND dc.table_name = table_name
-                AND dc.table_schema = 'fhir'
-            --GROUP BY key, value, data_type
-        )
-        SELECT vals.key as key,
-               CASE WHEN vals.data_type = 'ARRAY'
-                THEN json_array_to_array_literal(vals.value::json)
-                ELSE quote_literal(vals.value)
-              END AS value
-          FROM vals
-          UNION
-            SELECT 'id' AS key, quote_literal(id) AS value
-          UNION
-            SELECT 'parent_id' AS key, quote_literal(parent_id) AS value
-            WHERE parent_id IS NOT NULL
-          UNION
-            SELECT 'resource_id' AS key, quote_literal(resource_id) AS value
-            WHERE parent_id IS NOT NULL AND resource_id <> id
-      ) b;
+   || 'fhir.' || _table_name
+   || ' (' || string_agg(fhir.underscore(key),',') || ') '
+   || ' VALUES (' || string_agg(value, ',') || ')'
+   FROM key_vals b;
 $$ LANGUAGE sql VOLATILE;
 
 select meta.eval_ddl(
@@ -77,6 +78,9 @@ select meta.eval_ddl(
            ) _
       ), $q$
       || array_to_string(array_agg(cte),',')
+      || E'\n'
+      || 'SELECT resource_id, uuid, path, parent_id, value from patient '
+      || E'\nUNION ALL'
       || E'\n'
       || array_to_string(array_agg(slct),E'\nUNION ALL\n')
       || E';\n$$ LANGUAGE sql;'
@@ -108,15 +112,24 @@ FROM
   order by path
 ) _;
 
+
+--{{{
 \set pt_json `cat ../test/fixtures/patient.json`
 
 SELECT
-meta.eval_insert(
-  build_insert_satement(fhir.table_name(path)::text, value, id::text, parent_id::text, resource_id::text)
-)
+  meta.eval_insert(
+    build_insert_satement(
+      fhir.table_name(path)::text,
+      value,
+      id::text,
+      parent_id::text,
+      resource_id::text
+    )
+  )
 FROM insrt(:'pt_json'::json)
 where value is not null
 order by path
+limit 1
 ;
 
 --}}}
